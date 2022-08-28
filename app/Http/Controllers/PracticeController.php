8939e88b23fa4da844726d59cd4a5e3231568437
伊神徹 <ikamitooru@MacBook-Pro-5.local>
2022-08-29 05:11:37 +0900

    コメント

---
+++ app/Http/Controllers/PracticeController.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ namespace App\Http\Controllers;
+ 
+ class PracticeController extends Controller
+ {
+ public function sample()
+ {
+ return view('practice');
+ }
+ 
+ public function sample2()
+ {
+ $test = 'practice2';
+ return view('practice2', ['testParam' => $test]);
+ }
+ }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+ Route::get('practice', function() {
+ return response('practice');
+ });
+
+ Route::get('practice2', function() {
+    $test = 'practice2';
+    return response($test);
+    });
+
+ Route::get('practice3', function() {
+    $test = 'test';
+    return response($test);
+    });
+
+ use App\Http\Controllers\PracticeController;
+ 
+ // Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
+ Route::get('/practice', [PracticeController::class, 'sample']);
+ Route::get('/practice2', [PracticeController::class, 'sample2']);
